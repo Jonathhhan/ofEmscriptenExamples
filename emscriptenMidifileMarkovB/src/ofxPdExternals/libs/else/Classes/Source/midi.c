298facc3e643860d51fd0466059d1fec748f17bd
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-02-25 03:49:03 +0100

    Add files via upload

--- emscriptenMidifileMarkovB/src/ofxPdExternals/libs/else/Classes/Source/midi.c
+++ emscriptenMidifileMarkovB/src/ofxPdExternals/libs/else/Classes/Source/midi.c
@@ -63,7 +63,7 @@ typedef struct _midi{
     t_clock       *x_clock;
     t_clock       *x_slaveclock;
     t_outlet      *x_bangout;
-
+    t_outlet      *x_bangout_eom;
 // panic
     unsigned char  x_note_status;
     unsigned char  x_channel;
@@ -534,15 +534,14 @@ static void midi_dump_delta(t_midi *x){
     while(nevents--){  // LATER rethink sysex continuation
         unsigned char *bp = ep->e_bytes;
         double dp = ep->e_delta;
-        int size = strlen((char*) bp);
-	t_atom at[size + 1];
-	SETFLOAT(at, (t_float)dp);
-	SETFLOAT(at + 1, (t_float)*bp);
-	int i;
-	for(i = 0, bp++; i < size -1 && *bp != MIDI_EOM; i++, bp++)
-		SETFLOAT(at + i + 2, (t_float)*bp);
-        outlet_list(((t_object *)x)->ob_outlet, &s_list, size, at);
-	ep++;
+        outlet_float(((t_object *)x)->ob_outlet, (float)dp);
+        outlet_float(((t_object *)x)->ob_outlet, (float)*bp);
+        int i;
+        for(i = 0, bp++; i < 3 && *bp != MIDI_EOM; i++, bp++)
+            outlet_float(((t_object *)x)->ob_outlet, (float)*bp);
+            	if (*bp == MIDI_EOM)
+	    outlet_bang(x->x_bangout_eom);
+        ep++;
     }
     outlet_bang(x->x_bangout);
 }
@@ -981,6 +980,7 @@ static void *midi_new(t_symbol * s, int ac, t_atom *av){
     x->x_clock = clock_new(x, (t_method)midi_clocktick);
     x->x_slaveclock = clock_new(x, (t_method)midi_slaveclocktick);
     outlet_new((t_object *)x, &s_anything);
+    x->x_bangout_eom = outlet_new((t_object *)x, &s_bang);
     x->x_bangout = outlet_new((t_object *)x, &s_bang);
 // panic
     x->x_note_status = 0;
@@ -1012,3 +1012,4 @@ void midi_setup(void){
     class_addmethod(midi_class, (t_method)midi_speed, gensym("speed"), A_FLOAT, 0);;
     elsefile_setup();
 }
+
