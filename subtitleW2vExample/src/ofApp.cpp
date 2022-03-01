08503b849e61336710c78fc56672020f4ab9aba7
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-03-01 01:13:02 +0100

    Add files via upload

--- subtitleW2vExample/src/ofApp.cpp
+++ subtitleW2vExample/src/ofApp.cpp
@@ -2,91 +2,119 @@
 
 //--------------------------------------------------------------
 void ofApp::setup() {
-	string file_name = "vec_text8.bin";	
-	cout << "Loading embeddings file: " << file_name << endl;
-	embed.load_binary(file_name);
-	cout << "words: " << embed.words << endl;
-	cout << "dimensions: " << embed.size << endl;
+videoPlayer.load("Alphaville.mp4");
+videoPlayer.play();
+	movieTime = 0;
+	selectSubtitle = 0;
+	fileName = "vec_text8.bin";	
+	std::cout << "Loading embeddings file: " << fileName << std::endl;
+	embed.load_binary(fileName);
+	std::cout << "Words in " << fileName << ": " << embed.words << std::endl;
+	std::cout << "Dimensions in " << fileName << ": " <<  embed.size << std::endl;
 	subParserFactory = new SubtitleParserFactory(ofToDataPath("Alphaville.ENG.srt"));
 	parser = subParserFactory->getParser();
 
 	//to get subtitles
 	sub = parser->getSubtitles();
+	std::cout << "Subtitles: " << sub.size() << std::endl;
+	
+	// exclude those words
+	stopWords = {"i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"};
+	for (float i = 0; i < sub.size(); ++i) {
+		subIndex.insert(i);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    videoPlayer.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
-	vector<int> used_indices;
-	for (SubtitleItem* element : sub) {
+	ofSetColor(200);
+	ofDrawRectangle(90, 10, 420, 320);
+	ofSetColor(255);
+	videoPlayer.draw(100, 20, 400, 300);
+	if (sub[selectSubtitle]->getStartTime() <= movieTime + ofGetElapsedTimeMillis() && sub[selectSubtitle]->getEndTime() >= movieTime + ofGetElapsedTimeMillis()) {
+		ofSetColor(255, 200, 200);
+		ofDrawBitmapString(sub[selectSubtitle] -> getDialogue(), 300 - (sub[selectSubtitle] -> getDialogue().size()*4) , 360);
+	} else if (sub[selectSubtitle] -> getEndTime() + 1000 <= movieTime + ofGetElapsedTimeMillis()) {
 		std::map<int, int> m;
-		if (element->getStartTime() <= (ofGetElapsedTimeMillis() + 35000) && element->getEndTime() >= (ofGetElapsedTimeMillis() + 35000)) {
-			ofSetColor(255, 200, 200);
-			ofDrawBitmapString(element->getDialogue(), 50, 30);
-			ofSetColor(200, 255, 200);
-			ofDrawBitmapString(s3, 50, 80);
-			if (subNo != element->getSubNo()) {
-				s = "";
-				subNo = element->getSubNo();
-	    			word = element->getIndividualWords();
-            			for(std::string display : word){
-					
-					// make everything lower case
-    					std::for_each(display.begin(), display.end(), [](char & c) {
-        					c = ::tolower(c);
-    					});
-					
-					// erase those chars
-					char chars[] = ".,-!:?0123456789; ";
-					for (unsigned int i = 0; i < strlen(chars); ++i) {
-						display.erase (std::remove(display.begin(), display.end(), chars[i]), display.end());
-					}
-					
-					// exclude those words
-					std::string s1 = display.substr(0, display.find("'"));
-					if (s1 != "you" && s1 != "i" && s1 != "the" && s1 != "and" && s1 != "this" && s1 != "if" && s1 != "or" && s1 != "my" && s1 != "that" && s1 != "your" && s1 != "me" && s1 != "a" && s1 != "to" && s1 != "was" && s1 != "are"  && s1 != "it" && s1 != "with" && s1 != "for" && s1 != "in" && s1 != "is" && s1 != "of" && s1 != "von" && s1 != "on" && s1 != "braun" && s1 != "sir" && s1 != "alphaville") {
-						s.append (s1 + " + ");
-					}
+		std::multimap<int, int> m2;
+		std::vector<int> used_indices;
+		std::vector<string> v1, v2;
+		std::vector<int> choosenSubs;
+	    	individualWords = sub[selectSubtitle] -> getIndividualWords();
+	    	
+	    	// find next subtitle
+            	for(std::string individualWord : individualWords) {
+            		if (std::find(stopWords.begin(), stopWords.end(), individualWord) != stopWords.end()) {
+            			// do nothing
+            		} else {
+            		
+            			// make everything lower case
+    				std::for_each(individualWord.begin(), individualWord.end(), [](char & c) {
+        				c = std::tolower(c);
+    				});
+    				
+    				// erase those chars
+				char chars[] = ".,-!:?0123456789; ";
+				for (int i = 0; i < strlen(chars); ++i) {
+					individualWord.erase (std::remove(individualWord.begin(), individualWord.end(), chars[i]), individualWord.end());
 				}
-				s.pop_back();
-				s.pop_back();
-				s.pop_back();
+				individualWord = individualWord.substr(0, individualWord.find("'"));
 				
 				// create vector from words
-            			ofxWord2VecVector Vec = embed.words_to_vec(s, &used_indices);
+				ofxWord2VecVector Vec = embed.words_to_vec(individualWord, &used_indices);
 				if (!Vec.empty()) {
-					s2 = "";
-					int count = 200;
+					int count = 50;
 					auto match = embed.match_cos(Vec, count, used_indices);
-					cout << "Result:" << endl;
 					for (int i = 0; i < match.size(); i++) {
-						s2.append(match[i].word + " ");
-					}
-					
-					// find subtitle with most common w2v words
-					for (SubtitleItem* element : sub) {
-						vector<string> v1, v2;
-						v2 = element->getIndividualWords();
-						string temp;
-						std::stringstream stringstream1(s2);
-						while (stringstream1 >> temp)
-							v1.push_back(temp);
-						int counter = 0;
-						std::sort(v1.begin(), v1.end());
-						std::sort(v2.begin(), v2.end());
-						std::vector<string> v_intersection;
-						std::set_intersection(v1.begin(), v1.end(),
-						v2.begin(), v2.end(),
-						std::back_inserter(v_intersection));
-						for(string n : v_intersection)
-							counter++;
-						m[element->getSubNo()] = counter;
-					}
-					std::map<int,int>::iterator best = std::max_element(m.begin(),m.end(),[] (const std::pair<char,int>& a, const std::pair<char,int>& b)->bool{ return a.second < b.second; } );
-					std::cout << best->first << " , " << best->second << "\n";
-					std::cout << element->getDialogue() << "\n";
-					std::cout << sub[best->first]->getDialogue() << "\n";	
-					s3 = sub[best->first]->getDialogue();
+						v1.push_back(match[i].word);
+					}	
 				}
+			}
+		}
+		
+		// find subtitle with most common w2v words
+		for (float element : subIndex) {
+			v2 = sub[element] -> getIndividualWords();
+			std::sort(v1.begin(), v1.end());
+			std::sort(v2.begin(), v2.end());
+			std::vector<string> v_intersection;
+			std::set_intersection(v1.begin(), v1.end(),
+			v2.begin(), v2.end(),
+			std::back_inserter(v_intersection));
+			int counter = 0;
+			for(std::string n : v_intersection) {
+				counter++;
+			}
+			m[sub[element] -> getSubNo()] = (counter * 100000) / sub[element] -> getWordCount();
+		}
+		for (auto&& i : m) {
+    			m2.insert(std::make_pair(i.second, i.first));
+    		}
+		auto it = m2.rbegin(); // get the elem with the highest key
+		auto range = m2.equal_range(it->first);
+		for (auto it = range.first; it != range.second; ++it) {
+    			std::cout << "Weight: " << it -> first<< ", Subtitle: " << it -> second << std::endl; 
+    			choosenSubs.push_back(it -> second);
+    		}
+    		int random = rand() % choosenSubs.size();
+		selectSubtitle = choosenSubs[random] - 1;
+		std::cout << "Selected subtitle: " << selectSubtitle << std::endl;
+		std::cout << "Dialog: " << sub[selectSubtitle] -> getDialogue()<<std::endl;
+		videoPlayer.setPosition((sub[selectSubtitle - 1] -> getEndTime() / 1000) / videoPlayer.getDuration());
+		movieTime = sub[selectSubtitle - 1] -> getEndTime();
+		ofResetElapsedTimeCounter();
+		
+		// exclude choosen subtitle
+		for(auto it = subIndex.begin(); it != subIndex.end(); ) {
+			if(*it == selectSubtitle) {
+				it = subIndex.erase(it);
+			} else {
+				++it;
 			}
 		}
 	}
--- subtitleW2vExample/src/ofApp.h
+++ subtitleW2vExample/src/ofApp.h
@@ -5,19 +5,21 @@
 #include "ofxWord2Vec.h"
 
 class ofApp : public ofBaseApp{
-	
 	public:
-		
 		void setup();
+		void update();
 		void draw();
-
+		
+		ofVideoPlayer videoPlayer;
 		SubtitleParserFactory* subParserFactory;
 		SubtitleParser* parser;
 		std::vector<SubtitleItem*> sub;
-		int subNo;
-		string s2;
-		string s3;
-		string s;
-		 std::vector<std::string> word;
 		ofxWord2VecEmbedding embed;
+		
+		int movieTime;
+		int selectSubtitle;
+		std::string fileName;
+		std::set<int> subIndex;
+		std::vector<std::string> individualWords;
+		std::vector<std::string> stopWords;
 };
