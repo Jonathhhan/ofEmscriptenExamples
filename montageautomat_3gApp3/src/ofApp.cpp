4bf1a2d4a93f4bd653a5614e21a1d884eeb362e0
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-04-03 03:43:54 +0200

    Update ofApp.cpp

--- montageautomat_3gApp3/src/ofApp.cpp
+++ montageautomat_3gApp3/src/ofApp.cpp
@@ -99,6 +99,7 @@ void ofApp::bang_5onMousePressed(bool& e) {
 //--------------------------------------------------------------
 void ofApp::bang_4onMousePressed(bool& e) {
 	if (videoPlayerVector[numberOfVideoPlayer] -> isLoaded()) {
+		videoPlayerVector[numberOfVideoPlayer] -> setPaused(true);
 		mapSubVectorCopy = mapSubVector;
 		if ((subVector.size() > 0 && !bCustomWords) || (subVector.size() > 0 && customWords.empty())) {
 			if (bRandomStart) {
