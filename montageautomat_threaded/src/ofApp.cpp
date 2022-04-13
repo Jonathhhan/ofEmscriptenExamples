4d09498ce296742d991316136915d37accfa917f
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-04-13 18:23:28 +0200

    Update ofApp.cpp

--- montageautomat_threaded/src/ofApp.cpp
+++ montageautomat_threaded/src/ofApp.cpp
@@ -270,7 +270,7 @@ void ofApp::setup() {
 	groupOfBangs[4].setup(190, 330 + 5, 20);
 	groupOfToggles[3].setup(190, 380 + 5, 20);
 
-	stopWords = { "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "d", "do", "m", "re", "ll", "didn", "doesn", "hasn", "hadn", "cannot", "mustn", "isn", "wasn", "couldn", "wouldn", "mr", "ve", "l", "y", "to", "o", "m", "lsn", "from", "out" };
+	stopWords = {"ll", "re", "d", "m", "t", "s", "re"};
 }
 
 //--------------------------------------------------------------
