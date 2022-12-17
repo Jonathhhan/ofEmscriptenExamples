171e3f2230f6db5d5a41f1fb3d1e6ad25b23d7e1
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2022-12-17 06:21:31 +0100

    Add files via upload

--- webmidi_draft/src/ofApp.cpp
+++ webmidi_draft/src/ofApp.cpp
@@ -2,39 +2,13 @@
 
 unique_ptr<ofxDropdown> midiInputDropdown;
 unique_ptr<ofxDropdown> midiOutputDropdown;
-	
-void loadMidiInDevices(std::string string){  
-	std::vector<std::string> deviceList = ofSplitString(string, ",", true);      	
-	midiInputDropdown->clear();
-	for(int j = 0; j < deviceList.size(); j += 2){
-		midiInputDropdown->add(deviceList[j], deviceList[j + 1]);
-	}
-	midiInputDropdown->forceRedraw();
-}
-
-void loadMidiOutDevices(std::string string){  
-	std::vector<std::string> deviceList = ofSplitString(string, ",", true);      	
-	midiOutputDropdown->clear();
-	for(int j = 0; j < deviceList.size(); j += 2){
-		midiOutputDropdown->add(deviceList[j], deviceList[j + 1]);
-	}
-	midiOutputDropdown->forceRedraw();
-}
-
-void midiIn(std::string midiIn){
-	unsigned char* uMidiIn = (unsigned char*)midiIn.c_str();
-	int channel= uMidiIn[0];
-	int pitch= uMidiIn[1];
-	int velocity= uMidiIn[2];
-	std::cout << "midiin - channel: " << channel << " pitch: " << pitch << " velocity: " << velocity << std::endl;
-}
 
 //--------------------------------------------------------------
-void ofApp::setup() {
+void ofApp::setup(){
 	ofAddListener(toggle_1.onMousePressed, this, &ofApp::toggle_1_event);
-	emscripten::function("loadMidiInDevices", &loadMidiInDevices);
-	emscripten::function("loadMidiOutDevices", &loadMidiOutDevices);
-	emscripten::function("midiIn", &midiIn, emscripten::allow_raw_pointers());
+	emscripten::function("loadMidiInDevices", this->loadMidiInDevices);
+	emscripten::function("loadMidiOutDevices", this->loadMidiOutDevices);
+	emscripten::function("midiIn", this->midiIn);
 
 	gui.setup("midi options");
 	gui.setPosition(50, 50);
@@ -69,7 +43,7 @@ void ofApp::setup() {
 	settings.setOutListener(this);
 	ofSoundStreamSetup(settings);
 
-	if(!puredata.init(2, numInputs, 44100, ticksPerBuffer, false)) {
+	if(!puredata.init(2, numInputs, 44100, ticksPerBuffer, false)){
 		OF_EXIT_APP(1);
 	}
 	puredata.subscribe("midi_out");
@@ -80,18 +54,47 @@ void ofApp::setup() {
 }
 
 //--------------------------------------------------------------
-void ofApp::draw() {
+void ofApp::draw(){
 	gui.draw();
 	toggle_1.draw();
 }
 
 //--------------------------------------------------------------
-void ofApp::selectMidiIn(string &file) {
+void ofApp::loadMidiInDevices(std::string string){  
+	std::vector<std::string> deviceList = ofSplitString(string, ",", true);      	
+	midiInputDropdown->clear();
+	for(int j = 0; j < deviceList.size(); j += 2){
+		midiInputDropdown->add(deviceList[j], deviceList[j + 1]);
+	}
+	midiInputDropdown->forceRedraw();
+}
+
+//--------------------------------------------------------------
+void ofApp::loadMidiOutDevices(std::string string){  
+	std::vector<std::string> deviceList = ofSplitString(string, ",", true);      	
+	midiOutputDropdown->clear();
+	for(int j = 0; j < deviceList.size(); j += 2){
+		midiOutputDropdown->add(deviceList[j], deviceList[j + 1]);
+	}
+	midiOutputDropdown->forceRedraw();
+}
+
+//--------------------------------------------------------------
+void ofApp::midiIn(std::string midiIn){
+	unsigned char* uMidiIn = (unsigned char*)midiIn.c_str();
+	int channel= uMidiIn[0];
+	int pitch= uMidiIn[1];
+	int velocity= uMidiIn[2];
+	std::cout << "midiin - channel: " << channel << " pitch: " << pitch << " velocity: " << velocity << std::endl;
+}
+
+//--------------------------------------------------------------
+void ofApp::selectMidiIn(string &file){
 	EM_ASM(selectMidiIn(UTF8ToString($0)), file.c_str());
 }
 
 //--------------------------------------------------------------	      
-void ofApp::selectMidiOut(string &file) {
+void ofApp::selectMidiOut(string &file){
 	EM_ASM(selectMidiOut(UTF8ToString($0)), file.c_str());
 }
 
@@ -100,24 +103,25 @@ void ofApp::toggle_1_event(bool & e){
 	puredata.sendFloat(patch.dollarZeroStr() + "-toggle", e);
 }
 
-void ofApp::receiveList(const std::string &dest, const pd::List &list) {
-	if (dest == "midi_out") {
-		if (list.len() == 2) {
+//--------------------------------------------------------------
+void ofApp::receiveList(const std::string &dest, const pd::List &list){
+	if(dest == "midi_out"){
+		if(list.len() == 2){
 			int channel = list.getFloat(0);
 			int pitch = list.getFloat(1);
 			int array[2] = {channel, pitch};
 			size_t lengthOfArray = sizeof array / sizeof array[0];
-			EM_ASM_(
+			EM_ASM(
 			var data = new Uint32Array(HEAPU32.buffer, $0, $1);
 			sendMidi(data), array, lengthOfArray);
 			std::cout << "midiout - channel: " << channel << " pitch: " << pitch << std::endl;
-		} else if (list.len() == 3) {
+		}else if(list.len() == 3){
 			int channel = list.getFloat(0);
 			int pitch = list.getFloat(1);
 			int velocity = list.getFloat(2);
 			int array[3] = {channel, pitch, velocity};
 			size_t lengthOfArray = sizeof array / sizeof array[0];
-			EM_ASM_(
+			EM_ASM(
 			var data = new Uint32Array(HEAPU32.buffer, $0, $1);
 			sendMidi(data), array, lengthOfArray);
 			std::cout << "midiout - channel: " << channel << " pitch: " << pitch << " velocity: " << velocity << std::endl;
@@ -126,16 +130,16 @@ void ofApp::receiveList(const std::string &dest, const pd::List &list) {
 }
 
 //--------------------------------------------------------------
-void ofApp::print(const std::string &message) {
+void ofApp::print(const std::string &message){
 	cout << message << endl;
 }
 
 //--------------------------------------------------------------
-void ofApp::audioReceived(float * input, int bufferSize, int nChannels) {
+void ofApp::audioReceived(float * input, int bufferSize, int nChannels){
 	puredata.audioIn(input, bufferSize, nChannels);
 }
 
 //--------------------------------------------------------------
-void ofApp::audioRequested(float * output, int bufferSize, int nChannels) {
+void ofApp::audioRequested(float * output, int bufferSize, int nChannels){
 	puredata.audioOut(output, bufferSize, nChannels);
 }
--- webmidi_draft/src/ofApp.h
+++ webmidi_draft/src/ofApp.h
@@ -15,21 +15,23 @@ class ofApp : public ofBaseApp, public PdReceiver, public PdMidiReceiver {
         void setup();
         void draw();
 
+	static void loadMidiInDevices(std::string string);
+	static void loadMidiOutDevices(std::string string);
+	static void midiIn(std::string string);
 	void selectMidiIn(string & file);
 	void selectMidiOut(string & file);
 
 	void toggle_1_event(bool & e);
 
-	void print(const std::string &message);
-	void receiveList(const std::string &dest, const pd::List &list);	
+	void receiveList(const std::string &dest, const pd::List &list);
+	void print(const std::string &message);	
 	void audioReceived(float * input, int bufferSize, int nChannels);
 	void audioRequested(float * output, int bufferSize, int nChannels);
-	
-	ofxGuiGroup gui;
 
 	ofParameter<string> midiInputOptions;
 	ofParameter<string> midiOutputOptions;
 
+	ofxGuiGroup gui;
 	toggle toggle_1;
 	ofxPd puredata;
 	Patch patch;
