a12fab8d6b787d853c013e7ac22b8aca69134af5
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2023-06-05 19:13:50 +0200

    Add files via upload

---
+++ emscriptenFboTrailsRecordVideoExample/src/ofApp.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "ofMain.h"
+#include "toggle.h"
+#include "emscripten.h"
+
+class ofApp : public ofBaseApp{
+	public:
+		void setup();
+		void update();
+		void draw();
+		
+		void drawFboTest(); // draws some test graphics into the two fbos
+		
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		void toggle_1_event(bool & e);
+		void toggle_2_event(bool & e);
+		
+		//8 bits red, 8 bits green, 8 bits blue, from 0 to 255 in 256 steps
+		ofFbo rgbaFbo; // with alpha
+		
+		//32 bits red, 32 bits green, 32 bits blue, from 0 to 1 in 'infinite' steps	
+		ofFbo rgbaFboFloat; // with alpha
+		
+		int fadeAmnt;
+		bool isDrawingTexture;
+		toggle toggle_1;
+		toggle toggle_2;
+};
